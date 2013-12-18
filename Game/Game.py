67807c9dcbee2b757f6ff1cf9d63ea2b715ee32b
gpont <gpont97@gmail.com>
2013-12-18 20:53:59 +0700

    Fucking obj_to_JSON func...

--- Game/Game.py
+++ Game/Game.py
@@ -1,38 +1,256 @@
-import math, json
+import math, json, random
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 
-MAP_WIDTH = 80
-MAP_HEIGHT = 45
+MAP_DUNG_WIDTH = 80
+MAP_DUNG_HEIGHT = 45
 
 ROOM_MAX_SIZE = 10
 ROOM_MIN_SIZE = 6
 MAX_ROOMS = 30
 MAX_ROOM_MONSTERS = 3
 
-# global contaner - pseudo global space
-class GameCont: pass
+KEY_UP = 38
+KEY_DOWN = 40
+KEY_LEFT = 37
+KEY_RIGHT = 39
 
-class Tile: pass
-class Rect: pass
-class Object: pass
-class Fighter: pass
-class BasicMonster: pass
+class Tile:
+	def __init__(self, blocked):
+		self.blocked = blocked
+		self.explored = False
 
-def is_blocked(x, y): pass
-def create_room(room): pass
-def create_h_tunnel(x1, x2, y): pass
-def create_v_tunnel(y1, y2, x): pass
-def make_map(): pass
-def place_objects(room): pass
-def render_all(): pass
-def player_move_or_attack(dx, dy): pass
+class Rect:
+	def __init__(self, x, y, w, h):
+		self.x1 = x
+		self.y1 = y
+		self.x2 = x + w
+		self.y2 = y + h
+	def center(self):
+		center_x = (self.x1 + self.x2) / 2
+		center_y = (self.y1 + self.y2) / 2
+		return (center_x, center_y)
+	def intersect(self, other):
+		return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+				self.y1 <= other.y2 and self.y2 >= other.y1)
+
+class Object:
+	def __init__(self, x, y, char, name, color, blocks=False, fighter=None, ai=None):
+		self.x = x
+		self.y = y
+		self.char = char
+		self.name = name
+		self.color = color
+		self.blocks = blocks
+		self.fighter = fighter
+		if self.fighter:  #let the fighter component know who owns it
+			self.fighter.owner = self
+		self.ai = ai
+		if self.ai:  #let the AI component know who owns it
+			self.ai.owner = self
+
+	def move(self, dx, dy):
+		if not is_blocked(self.x + dx, self.y + dy):
+			self.x += dx
+			self.y += dy
+
+	def move_towards(self, target_x, target_y):
+		#vector from this object to the target, and distance
+		dx = target_x - self.x
+		dy = target_y - self.y
+		distance = math.sqrt(dx ** 2 + dy ** 2)
+		dx = int(round(dx / distance))
+		dy = int(round(dy / distance))
+		self.move(dx, dy)
+
+	def distance_to(self, other):
+		dx = other.x - self.x
+		dy = other.y - self.y
+		return math.sqrt(dx ** 2 + dy ** 2)
+
+class Fighter:
+	def __init__(self, hp, defense, power):
+		self.max_hp = hp
+		self.hp = hp
+		self.defense = defense
+		self.power = power
+
+class BasicMonster:
+	# test AI for mosters
+	def take_turn(self):
+		#if you can see it, it can see you
+		monster = self.owner
+		
+		#move towards player if far away
+		if monster.distance_to(player) >= 2:
+			monster.move_towards(player.x, player.y)
+		
+		#close enough, attack! (if the player is still alive.)
+		elif player.fighter.hp > 0:
+			player.fighter.hp -= monster.fighter.power/player.fighter.defense
+
+def is_blocked(x, y):
+	print(x, y, type(map_dung[y][x]), dir(map_dung[y][x]))
+	if map_dung[y][x].blocked:
+		return True
+
+	for object in objects:
+		if object.blocks and object.x == x and object.y == y:
+			return True
+ 
+	return False
+
+def create_room(room):
+	global map_dung
+	for x in range(room.x1 + 1, room.x2):
+		for y in range(room.y1 + 1, room.y2):
+			map_dung[y][x].blocked = False
+
+def create_h_tunnel(x1, x2, y):
+	global map_dung
+	for x in range(min(x1, x2), max(x1, x2) + 1):
+		map_dung[y][x].blocked = False
+
+def create_v_tunnel(y1, y2, x):
+	global map_dung
+	for y in range(min(y1, y2), max(y1, y2) + 1):
+		map_dung[y][x].blocked = False
+
+def make_map_dung():
+	# dungeon generator
+	global map_dung, player
+
+	#fill map_dung with "blocked" tiles
+	map_dung = [[ Tile(True)
+		for x in range(MAP_DUNG_WIDTH) ]
+			for y in range(MAP_DUNG_HEIGHT) ]
+
+	rooms = []
+	num_rooms = 0
+	for r in range(MAX_ROOMS):
+		#random width and height
+		w = random.randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		h = random.randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		#random position without going out of the boundaries of the map_dung
+		x = random.randint(0, MAP_DUNG_WIDTH - w - 1)
+		y = random.randint(0, MAP_DUNG_HEIGHT - h - 1)
+
+		new_room = Rect(x, y, w, h)
+
+		failed = False
+		for other_room in rooms:
+			if new_room.intersect(other_room):
+				failed = True
+				break
+ 		
+		if not failed:
+			create_room(new_room)
+			place_objects(new_room)
+			(new_x, new_y) = new_room.center()
+			
+			if num_rooms == 0:
+				player.x = new_x
+				player.y = new_y
+			else:
+				(prev_x, prev_y) = rooms[num_rooms-1].center()
+ 			
+				if random.randint(0, 1) == 1:
+					create_h_tunnel(prev_x, new_x, prev_y)
+					create_v_tunnel(prev_y, new_y, new_x)
+				else:
+					create_v_tunnel(prev_y, new_y, prev_x)
+					create_h_tunnel(prev_x, new_x, new_y)
+			rooms.append(new_room)
+			num_rooms += 1
+
+def place_objects(room):
+	num_monsters = random.randint(0, MAX_ROOM_MONSTERS)
+ 
+	for i in range(num_monsters):
+		x = random.randint(room.x1, room.x2)
+		y = random.randint(room.y1, room.y2)
+
+		if not is_blocked(x, y):
+			if random.randint(0, 100) < 80:  #80% chance of getting an orc
+				#create an orc
+				fighter_component = Fighter(hp=10, defense=0, power=3)
+				ai_component = BasicMonster()
+ 
+				monster = Object(x, y, 'o', 'orc', 'green',
+					blocks=True, fighter=fighter_component, ai=ai_component)
+			else:
+				#create a troll
+				fighter_component = Fighter(hp=16, defense=1, power=4)
+				ai_component = BasicMonster()
+ 
+				monster = Object(x, y, 'T', 'troll', 'darker_green',
+					blocks=True, fighter=fighter_component, ai=ai_component)
+ 
+			objects.append(monster)
+
+def obj_to_JSON(obj):
+	if str(type(obj)) == "<type 'instance'>":
+		print(obj, obj.__dict__)
+		return obj_to_JSON(obj.__dict__)
+	elif type(obj) == list:
+		for i in range(len(obj)):
+			obj[i] = obj_to_JSON(obj[i])
+	elif type(obj) == dict:
+		for key in obj:
+			obj[key] = obj_to_JSON(obj[key])
+	else:
+		return obj
+
+def render_all():
+	global sender_data
+	sender_data_arr = {}
+	sender_data_arr["map_dung"] = map_dung
+	sender_data_arr["objects"] = objects
+	obj_to_JSON(sender_data_arr)
+	sender_data = json.dumps(sender_data_arr)
+
+def player_move_or_attack(dx, dy):
+	x = player.x + dx
+	y = player.y + dy
+
+	target = None
+	for obj in objects:
+		if obj.x == x and obj.y == y:
+			target = obj
+			break
+
+	if target is not None:
+		target.fighter.hp -= player.fighter.power/target.fighter.defense
+	else:
+		player.move(dx, dy)
 
 sender_data = ""
 
 def init():
-	pass
+	global player
+	global objects
+	fighter_component = Fighter(hp=30, defense=2, power=5)
+	player = Object(0, 0, '@', 'player', "white", blocks=True, fighter=fighter_component)
+	objects = [player]
+	make_map_dung()
+
+def game_loop():
+	for object in objects:
+		if object.ai:
+			object.ai.take_turn()
+
+	render_all()
 
 def handle_keys(d):
-	pass
+	if d == str(KEY_UP):
+		player_move_or_attack(0, -1)
+	elif d == str(KEY_DOWN):
+		player_move_or_attack(0, 1)
+	elif d == str(KEY_LEFT):
+		player_move_or_attack(-1, 0)
+	elif d == str(KEY_RIGHT):
+		player_move_or_attack(1, 0)
+
+	game_loop()
+
